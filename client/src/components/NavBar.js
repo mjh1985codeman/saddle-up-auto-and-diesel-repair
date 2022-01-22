3d735267e5e168de58ac06b19eeec0dfe58209c4
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2022-01-21 22:54:30 -0600

    added header image

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -9,12 +9,15 @@ const AppNavBar = () => {
 
   return (
     <>
-      <div className="nav-style navbar-bkgd">
-        <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg">
+      <div className="nav-style navbar-bkgd nav-bkg-img">
+        <Navbar collapseOnSelect variant="dark" expand="lg">
           <Navbar.Brand>
             <img src={logo} alt="Logo" />
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Toggle
+            className="nav-toggle"
+            aria-controls="responsive-navbar-nav"
+          />
           <Navbar.Collapse id="responsive-navbar-nav">
             <div className="navbar-link text-center mt-4 mb-4">
               <Nav className="me-auto">
