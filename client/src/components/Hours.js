6802bbd41657076a7adc637ece3b6181f4deefb5
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2022-01-21 18:10:50 -0600

    basic hours of operations styling began

--- client/src/components/Hours.js
+++ client/src/components/Hours.js
@@ -1,9 +1,37 @@
 import React from "react";
-
+import { Container, Table, Card } from "react-bootstrap";
 const Hours = () => {
   return (
     <>
-      <p>This is where the Hours of operations will be!</p>
+      <div className="hours-comp">
+        <Container>
+          <h2>Hours of Operation:</h2>
+          <Table className="hours-tab-color">
+            <thead>
+              <tr>
+                <th>Day</th>
+                <th>Open</th>
+                <th>Close</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Tuesday - Friday</td>
+                <td>8:00am</td>
+                <td>5:00pm</td>
+              </tr>
+              <tr>
+                <td>Saturday</td>
+                <td>8:00am</td>
+                <td>6:00pm</td>
+              </tr>
+            </tbody>
+          </Table>
+          <Card className="text-center hours-tab-color">
+            <Card.Body>**Closed On Sunday and Monday**</Card.Body>
+          </Card>
+        </Container>
+      </div>
     </>
   );
 };
